dce5cffcbbfb7886d11bc8ad7cb2fb592d1808a1
Ravikachhadiya <ravikachhadiya@gmail.com>
2020-09-11 23:45:57 +0530

    FSJ0720A115

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -9,14 +9,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import model.*;
+import java.time.*;
 
 
 
 @WebServlet(urlPatterns= {"/blog"})
 public class BlogController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+	private static final long serialVersionUID = 1L;       
 
     public BlogController() {
         super();
@@ -32,19 +32,24 @@ public class BlogController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
+		String[] blogDetails = request.getParameter("selectedAnswers").split(",");
 		
-		
-		
+		LocalDate date = LocalDate.now();
+		Blog blog = new Blog(blogDetails[0], blogDetails[1], date);
+		LocalDateTime date1 = LocalDateTime.now();
 
-		
+		User user = new User("admin","admin",date1);
 		if(blog!=null) {
 			request.setAttribute("blog", blog);
-			request.setAttribute("user",user);
+			//request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
+			
+			System.out.println("Blog Title: " + blogDetails[0] + "\n" +
+					"Blog Description: " + blogDetails[1] +"\n" + 
+					"Posted on: " + date);
 		}
-	*/	
+		
 	}
 
 }

1fa084b1d0bc931026dc94c9e88752f0c64e5f45
RheaSudesh <vrhea304@gmail.com>
2021-01-19 21:48:41 +0530

    ProGrad-1528832

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import model.Blog;
+import model.User;
 
 
 
@@ -20,7 +24,6 @@ public class BlogController extends HttpServlet {
 
     public BlogController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 
@@ -32,8 +35,17 @@ public class BlogController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
+		String blogDetails = request.getParameter("selectedAnswers");
 		
+        String[] blogDetail=blogDetails.split(",");
+		
+		LocalDateTime date1 = LocalDateTime.of(2021, 1, 1, 1, 01, 1);
+		User user = new User ( "rhea@gmail.com","Cse@123",date1);
+		
+		LocalDate date2=LocalDate.of(2021,01,07);
+		
+		Blog blog=new Blog(blogDetail[0], blogDetail[1], date2);
+		System.out.println("Blog Title: "+blogDetail[0]+". "+"\nBlog Description"+blogDetail[1]+"\nPosted On: "+date2);
 		
 		
 
@@ -44,7 +56,7 @@ public class BlogController extends HttpServlet {
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+		
 	}
 
 }

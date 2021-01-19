734d00d9d6affbe00c8e30c9d71e5035fab6d8e5
Prashasti <agarwalprashasti1999@gmail.com>
2021-01-19 21:02:23 +0530

    Blog-II

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Blog;
+import model.User;
+
+
 
 
 
@@ -31,20 +35,30 @@ public class BlogController extends HttpServlet {
 	}
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	String blogDetails = request.getParameter("selectedAnswers");
+		String[] str=blogDetails.split(",");
+		String blogTitle=str[0];
+		String blogDescription=str[1];
+		LocalDate date=LocalDate.now();
+		Blog blog=new Blog(blogTitle,blogDescription, date );
+		User user = null;
 		
+		System.out.println(blogDetails);
+		blog.setBlogTitle(blogTitle);
+		blog.setBlogDescription(blogDescription);
+		blog.setDate(date);
+		System.out.println("Title: "+blogTitle);
+		 System.out.println("Description: "+blogDescription);
+		 System.out.println("Date: "+date);
 		
-		
-
-		
+		 
 		if(blog!=null) {
 			request.setAttribute("blog", blog);
 			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+		 
 	}
 
 }
--- src/model/Blog.java
+++ src/model/Blog.java
@@ -1,2 +1,35 @@
 package model;
-// Type your code
+
+import java.time.LocalDate;
+
+public class Blog{
+	String blogTitle;
+	String blogDescription;
+	LocalDate date;
+	public Blog(String blogTitle, String blogDescription, LocalDate date)
+	{
+		this.blogTitle=blogTitle;
+		this.blogDescription=blogDescription;
+		this.date=date;
+	}
+	public String getBlogTitle() {
+		return blogTitle;
+	}
+	public void setBlogTitle(String blogTitle) {
+		this.blogTitle = blogTitle;
+	}
+	public String getBlogDescription() {
+		return blogDescription;
+	}
+	public void setBlogDescription(String blogDescription) {
+		this.blogDescription = blogDescription;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
+}
